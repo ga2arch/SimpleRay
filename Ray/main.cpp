4ad58f7f131cad97f070cdc2d3a6b85b47850ca6
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-01-05 00:42:36 +0100

    tracing simple sphere

--- Ray/main.cpp
+++ Ray/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdio>
 
 // MATH
 struct Vec {
@@ -56,12 +57,12 @@ struct Ray {
     Ray(Vec o_, Vec d_): o(o_), d(d_) {};
 };
 
-inline bool quadratic(float A, float B, float C, float* t0, float* t1) {
-    float d = B * B - 4.f * A * C;
+inline bool quadratic(double A, double B, double C, double* t0, double* t1) {
+    double d = B * B - 4.0 * A * C;
     if (d <= 0) return false;
     
-    float r_d = sqrtf(d);
-    float q;
+    double r_d = sqrt(d);
+    double q;
     
     if (B < 0) q = -.5f * (B - r_d);
     else       q = -.5f * (B + r_d);
@@ -73,6 +74,10 @@ inline bool quadratic(float A, float B, float C, float* t0, float* t1) {
     return true;
 }
 
+inline double clamp(double x, double a=0, double b=1) {
+    return x < a ? a : (x > b ? b : x);
+}
+
 // SHAPES
 
 struct Sphere {
@@ -81,26 +86,54 @@ struct Sphere {
     
     Sphere(Vec p, Vec e, Vec c, double r): pos(p), emission(e), color(c), rad(r) {}
     
-    bool intersect(const Ray &ray, float *pos) {
-        float A = powf(ray.d.x, 2) + powf(ray.d.y, 2) + powf(ray.d.z, 2);
-        float B = 2 * (ray.d.x*ray.o.x + ray.d.y*ray.o.y + ray.d.z*ray.o.z);
-        float C = powf(ray.o.x, 2) + powf(ray.o.y, 2) + powf(ray.o.z, 2) - powf(rad, 2);
+    double intersect(const Ray &ray, double *t) {
+        auto A = dot(ray.d, ray.d);
+        auto B = 2 * dot(ray.d, ray.o);
+        auto C = dot(ray.o, ray.o) - rad*rad;
         
-        float t0, t1;
+        double t0, t1;
         if (!quadratic(A, B, C, &t0, &t1))
             return false;
         
-        *pos = t0;
+        *t = t0;
         return true;
     }
 };
 
+inline int to_int(double f) {
+    return clamp(f) * 255;
+}
 
 int main() {
     int w = 800, h = 600;
     
+    Ray cam(Vec(0, 0, -3), norm(Vec(0,0,0)));
+    Vec *c = new Vec[w*h];
     
+    Sphere s(Vec(0,2,0), Vec(), Vec(.6, 0, .2), 1);
     
+    for(int y=0; y < h; y++) {
+        for (int x=0; x < w; x++) {
+            int i = (h-y-1)*w+x;
+            
+            auto fovDist = .557;
+            auto xx = (2 * ((x + 0.5) * 1.0/w) - 1) * fovDist * w/h;
+            auto yy = (1 - 2 * ((y + 0.5) * 1.0/h)) * fovDist;
+            
+            Ray r(cam.o, norm(Vec(xx,yy,-1)));
+            
+            double t;
+            if (s.intersect(r, &t) && t <= 1000)
+                c[i] = s.color;
+            else
+                c[i] = Vec();
+        }
+    }
+    
+    FILE *f = fopen("image.ppm", "w");         // Write image to PPM file.
+    fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
+    for (int i=0; i<w*h; i++)
+        fprintf(f,"%d %d %d ", to_int(c[i].x), to_int(c[i].y), to_int(c[i].z));
 }
 
 
